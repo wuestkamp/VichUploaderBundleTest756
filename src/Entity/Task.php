58882d19df9b9486a72d22b5ceed331317468317
Kim <kim@wuestkamp.com>
2018-04-26 19:02:58 +0200

    task is a database entity

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -3,13 +3,30 @@
 namespace App\Entity;
 
 use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TaskRepository")
+ */
 class Task
 {
     /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
      * @Assert\NotBlank()
      */
     private $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
 
     /**
      * @return mixed
